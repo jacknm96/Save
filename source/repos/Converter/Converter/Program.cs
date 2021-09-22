2d6e66664ff787b58c2831d7f9a538ff712c5ba3
Jack <jacknm2020@gmail.com>
2021-09-21 17:53:31 -0700

    hi

---
+++ source/repos/Converter/Converter/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Converter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        static float[] ToPolar(float x, float y)
+        {
+            float[] coordinates = new float[2];
+            coordinates[0] = MathF.Sqrt(x * x + y * y);
+            coordinates[1] = MathF.Atan2(y, x);
+            return coordinates;
+        }
+
+        static float[] ToCartesian(float hyp, float rad)
+        {
+            float[] coordinates = new float[2];
+            coordinates[0] = MathF.Sin(rad) * hyp;
+            coordinates[1] = MathF.Cos(rad) * hyp;
+            return coordinates;
+        }
+    }
+}
