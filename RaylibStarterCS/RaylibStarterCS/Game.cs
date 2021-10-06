56e04f67d123944bd75f7f9535c47c8526a4143f
Jack <jacknm2020@gmail.com>
2021-10-06 12:49:01 -0700

    Finishing movement/adding bullets

--- RaylibStarterCS/RaylibStarterCS/Game.cs
+++ RaylibStarterCS/RaylibStarterCS/Game.cs
@@ -18,13 +18,27 @@ namespace RaylibStarterCS
 
         private float deltaTime = 0.005f;
 
-        Image logo;
-        Texture2D logoTexture;
+        Image barrel;
+        Texture2D barrelTexture;
+        float barrelRotation;
+        float barrelWidth;
+        float barrelHeight;
+        Rectangle sourceRecBarrel;
+        Rectangle destRecBarrel;
+        Vector2 barrelOrigin;
 
         Image tank;
         Texture2D tankTexture;
         Vector3 tankPosition;
+        Vector2 origin;
         float tankRotation;
+        float tankWidth;
+        float tankHeight;
+        Rectangle sourceRec;
+        Rectangle destRec;
+
+        Bullet[] bullets = new Bullet[10];
+        int numBullets = 0;
 
         float m_timer = 0;
 
@@ -44,12 +58,20 @@ namespace RaylibStarterCS
                 Console.WriteLine("Stopwatch high-resolution frequency: {0} ticks per second", Stopwatch.Frequency);
             }
 
-            logo = LoadImage("../Images/aie-logo-dark.png");
-            logoTexture = LoadTextureFromImage(logo);
+            barrel = LoadImage("../Images/barrelGreen.png");
+            barrelTexture = LoadTextureFromImage(barrel);
+            barrelWidth = barrel.width;
+            barrelHeight = barrel.height;
 
             tank = LoadImage("../Images/tankBlack.png");
             tankTexture = LoadTextureFromImage(tank);
-            tankPosition = new Vector3(GetScreenWidth() / 2 - 35, GetScreenHeight() / 2 - 37, 0);
+            tankWidth = tank.width;
+            tankHeight = tank.height;
+            sourceRec = new Rectangle( 0f, 0f, tankWidth, tankHeight );
+            sourceRecBarrel = new Rectangle(0f, 0f, barrelWidth, barrelHeight);
+            tankPosition = new Vector3(GetScreenWidth() / 2 - tankWidth, GetScreenHeight() / 2 - tankHeight, 0);
+            origin = new Vector2(tankWidth / 2, tankHeight / 2);
+            barrelOrigin = new Vector2(barrelWidth / 2, 0);
 
             SetTargetFPS(60);       // Set our game to run at 60 frames-per-second
 
@@ -80,21 +102,33 @@ namespace RaylibStarterCS
             // insert game logic here 
             if (IsKeyDown(KeyboardKey.KEY_W))
             {
-                tankPosition.Y -= 500.0f * deltaTime * MathF.Sin(tankRotation);
-                tankPosition.X -= 500.0f * deltaTime * MathF.Cos(tankRotation);
+                tankPosition.X -= 500.0f * deltaTime * MathF.Sin(tankRotation * MathF.PI / 180f);
+                tankPosition.Y += 500.0f * deltaTime * MathF.Cos(tankRotation * MathF.PI / 180f);
             }
 
             if (IsKeyDown(KeyboardKey.KEY_S))
             {
-                tankPosition.Y += 500.0f * deltaTime * MathF.Sin(tankRotation);
-                tankPosition.X += 500.0f * deltaTime * MathF.Cos(tankRotation);
+                tankPosition.X += 500.0f * deltaTime * MathF.Sin(tankRotation * MathF.PI / 180f);
+                tankPosition.Y -= 500.0f * deltaTime * MathF.Cos(tankRotation * MathF.PI / 180f);
             }
 
             if (IsKeyDown(KeyboardKey.KEY_A))
-                tankRotation += 50.0f * deltaTime;
+                tankRotation -= 250.0f * deltaTime;
 
             if (IsKeyDown(KeyboardKey.KEY_D))
-                tankRotation -= 50.0f * deltaTime;
+                tankRotation += 250.0f * deltaTime;
+
+            if (IsKeyDown(KeyboardKey.KEY_Q))
+                barrelRotation -= 250.0f * deltaTime;
+
+            if (IsKeyDown(KeyboardKey.KEY_E))
+                barrelRotation += 250.0f * deltaTime;
+
+            if (IsKeyDown(KeyboardKey.KEY_SPACE))
+            {
+                bullets[numBullets] = new Bullet(tankRotation + barrelRotation, new Vector2(tankPosition.X + barrelHeight * MathF.Cos(tankRotation + barrelRotation), tankPosition.Y + barrelHeight * MathF.Sin(tankRotation + barrelRotation)));
+                numBullets++;
+            }
 
             m_timer += deltaTime;
 
@@ -110,6 +144,9 @@ namespace RaylibStarterCS
 
             if (IsKeyDown(KeyboardKey.KEY_RIGHT))
                 camera.target.X += 500.0f * deltaTime;
+
+            destRec = new Rectangle(tankPosition.X + tankWidth, tankPosition.Y + tankHeight, tankWidth, tankHeight);
+            destRecBarrel = new Rectangle(tankPosition.X + tankWidth, tankPosition.Y + tankHeight, barrelWidth, barrelHeight);
 
 
             // Camera zoom controls
@@ -133,11 +170,23 @@ namespace RaylibStarterCS
 
                     ClearBackground(Color.WHITE);
 
-			/*DrawTexture(logoTexture,
+            /*DrawTexture(logoTexture,
 				GetScreenWidth() - logoTexture.width, 0, Color.WHITE);*/
 
             // demonstrate spinning sprite
-            DrawTextureEx(tankTexture, new Vector2(tankPosition.X, tankPosition.Y), tankRotation, 1, Color.WHITE);
+            //DrawTextureEx(tankTexture, new Vector2(tankPosition.X, tankPosition.Y), tankRotation, 1, Color.WHITE);
+            DrawTexturePro(tankTexture, sourceRec, destRec, origin, tankRotation, Color.WHITE);
+
+            DrawTexturePro(barrelTexture, sourceRecBarrel, destRecBarrel, barrelOrigin, barrelRotation + tankRotation, Color.WHITE);
+
+            foreach (Bullet bullet in bullets)
+            {
+                if (bullet != null)
+                {
+                    DrawTextureEx(bullet.bulletTexture, bullet.bulletLocation, bullet.bulletRotation, 1, Color.WHITE);
+                }
+            }
+            //DrawTextureEx(barrelTexture, new Vector2(destRec.x, destRec.y), tankRotation, 1, Color.WHITE);
 
             /* draw a thin line
             DrawLine(300, 300, 600, 400, Color.BLACK);
@@ -155,5 +204,21 @@ namespace RaylibStarterCS
             EndDrawing();
         }
 
+    }
+
+    public class Bullet
+    {
+        Image bullet;
+        public Texture2D bulletTexture;
+        public float bulletRotation;
+        public Vector2 bulletLocation;
+
+        public Bullet(float rotation, Vector2 position)
+        {
+            bullet = LoadImage("../Images/bulletYellow.png");
+            bulletTexture = LoadTextureFromImage(bullet);
+            bulletRotation = rotation;
+            bulletLocation = position;
+        }
     }
 }
