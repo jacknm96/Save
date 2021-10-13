fcc676fa595549d6b8e65b8780ef9274e3caa3f6
jacknm96 <jacknm2020@gmail.com>
2021-10-13 10:05:39 -0700

    Im Dumb

--- source/repos/ConsoleApp2/Matrix3.cs
+++ source/repos/ConsoleApp2/Matrix3.cs
@@ -4,9 +4,9 @@ namespace MathClasses
 {
     public class Matrix3
     {
-        public float m00, m01, m02;
-        public float m10, m11, m12;
-        public float m20, m21, m22;
+        public float m00, m10, m20;
+        public float m01, m11, m21;
+        public float m02, m12, m22;
 
         // default constructor creates the identity matrix
         public Matrix3()
@@ -18,25 +18,25 @@ namespace MathClasses
 
         public Matrix3(float a)
         {
-            m00 = 1f;
-            m11 = 1f;
-            m22 = 1f;
+            m00 = a;
+            m11 = a;
+            m22 = a;
         }
 
         // takes in 3 vector3s to use as its columns
-        public Matrix3(Vector3 first, Vector3 second, Vector3 third)
+        public Matrix3(Vector3 col1, Vector3 col2, Vector3 col3)
         {
-            m00 = first.x; m01 = second.x; m02 = third.x;
-            m10 = first.y; m11 = second.y; m12 = third.y;
-            m20 = first.z; m21 = second.z; m22 = third.z;
+            m00 = col1.x; m01 = col1.y; m02 = col1.z;
+            m10 = col2.x; m11 = col2.y; m12 = col2.z;
+            m20 = col3.x; m21 = col3.y; m22 = col3.z;
         }
 
         // takes in 9 floats to use as parameters. floats must be in column-major order
         public Matrix3(float m1, float m2, float m3, float m4, float m5, float m6, float m7, float m8, float m9)
         {
-            m00 = m1; m01 = m4; m02 = m7;
-            m10 = m2; m11 = m5; m12 = m8;
-            m20 = m3; m21 = m6; m22 = m9;
+            m00 = m1; m01 = m2; m02 = m3;
+            m10 = m4; m11 = m5; m12 = m6;
+            m20 = m7; m21 = m8; m22 = m9;
         }
 
         public void WriteMatrix()
@@ -46,28 +46,25 @@ namespace MathClasses
             Console.WriteLine(m20 + " " + m21 + " " + m22);
         }
 
-        public Matrix3 SetRotateX(float a)
+        public void SetRotateX(float a)
         {
-            return this * new Matrix3(
-                new Vector3(1, 0, 0),
-                new Vector3(0, (float)Math.Cos(a), -(float)Math.Sin(a)),
-                new Vector3(0, (float)Math.Sin(a), (float)Math.Cos(a)));
+            m00 = 1; m10 = 0; m20 = 0;
+            m01 = 0; m11 = MathF.Cos(a); m21 = -MathF.Sin(a);
+            m02 = 0; m12 = MathF.Sin(a); m22 = MathF.Cos(a);
         }
 
-        public Matrix3 SetRotateY(float a)
+        public void SetRotateY(float a)
         {
-            return this * new Matrix3(
-                new Vector3((float)Math.Cos(a), 0, (float)Math.Sin(a)),
-                new Vector3(0, 1, 0),
-                new Vector3(-(float)Math.Sin(a), 0, (float)Math.Cos(a)));
+            m00 = MathF.Cos(a); m10 = 0; m20 = MathF.Sin(a);
+            m01 = 0; m11 = 1; m21 = 0;
+            m02 = -MathF.Sin(a); m12 = 0; m22 = MathF.Cos(a);
         }
 
-        public Matrix3 SetRotateZ(float a)
+        public void SetRotateZ(float a)
         {
-            return this * new Matrix3(
-                new Vector3((float)Math.Cos(a), -(float)Math.Sin(a), 0),
-                new Vector3((float)Math.Sin(a), (float)Math.Cos(a), 0),
-                new Vector3(0, 0, 1));
+            m00 = MathF.Cos(a); m10 = -MathF.Sin(a); m20 = 0;
+            m01 = MathF.Sin(a); m11 = MathF.Cos(a); m21 = 0;
+            m02 = 0; m12 = 0; m22 = 1;
         }
 
         public void SetTranslation(float a, float b)
@@ -86,24 +83,19 @@ namespace MathClasses
         // vector transformation
         public static Vector3 operator *(Matrix3 m, Vector3 v)
         {
-            return new Vector3(v.x * m.m00 + v.y * m.m01 + v.z * m.m02,
-                v.x * m.m10 + v.y * m.m11 + v.z * m.m12,
-                v.x * m.m20 + v.y * m.m21 + v.z * m.m22);
+            return new Vector3(v.Dot(new Vector3(m.m00, m.m10, m.m20)),
+                v.Dot(new Vector3(m.m01, m.m11, m.m21)),
+                v.Dot(new Vector3(m.m02, m.m12, m.m22)));
         }
 
         // matrix multiplication
         public static Matrix3 operator *(Matrix3 m1, Matrix3 m2)
         {
-            Vector3 row1 = new Vector3(m1.m00, m1.m01, m1.m02);
-            Vector3 row2 = new Vector3(m1.m10, m1.m11, m1.m12);
-            Vector3 row3 = new Vector3(m1.m20, m1.m21, m1.m22);
-            Vector3 col1 = new Vector3(m2.m00, m2.m10, m2.m20);
-            Vector3 col2 = new Vector3(m2.m01, m2.m11, m2.m21);
-            Vector3 col3 = new Vector3(m2.m02, m2.m12, m2.m22);
-
-            return new Matrix3(new Vector3(row1.Dot(col1), row2.Dot(col1), row3.Dot(col1)),
-                new Vector3(row1.Dot(col2), row2.Dot(col2), row3.Dot(col2)),
-                new Vector3(row1.Dot(col3), row2.Dot(col3), row3.Dot(col3)));
+            
+            Vector3 col1 = new Vector3(m2.m00, m2.m01, m2.m02);
+            Vector3 col2 = new Vector3(m2.m10, m2.m11, m2.m12);
+            Vector3 col3 = new Vector3(m2.m20, m2.m21, m2.m22);
+            return new Matrix3(m1 * col1, m1 * col2, m1 * col3);
         }
     }
 }
--- source/repos/ConsoleApp2/Matrix4.cs
+++ source/repos/ConsoleApp2/Matrix4.cs
@@ -19,21 +19,24 @@ namespace MathClasses
         }
 
         // takes in 4 vector4s to use as its columns
-        public Matrix4(Vector4 first, Vector4 second, Vector4 third, Vector4 fourth)
+        public Matrix4(Vector4 col1, Vector4 col2, Vector4 col3, Vector4 col4)
         {
-            m00 = first.x; m01 = second.x; m02 = third.x; m03 = third.x;
-            m10 = first.y; m11 = second.y; m12 = third.y; m13 = third.y;
-            m20 = first.z; m21 = second.z; m22 = third.z; m23 = third.z;
-            m30 = first.w; m31 = second.w; m32 = third.w; m33 = third.w;
+            m00 = col1.x; m01 = col1.y; m02 = col1.z; m03 = col1.w;
+            m10 = col2.x; m11 = col2.y; m12 = col2.z; m13 = col2.w;
+            m20 = col3.x; m21 = col3.y; m22 = col3.z; m23 = col3.w;
+            m30 = col4.x; m31 = col4.y; m32 = col4.z; m33 = col4.w;
         }
 
         // takes in 16 floats to use as parameters. floats must be in column-major order
-        public Matrix4(float m1, float m2, float m3, float m4, float m5, float m6, float m7, float m8, float m9, float m101, float m111, float m121, float m131, float m14, float m15, float m16)
+        public Matrix4(float one, float two, float three, float four, 
+            float five, float six, float seven, float eight, 
+            float nine, float ten, float eleven, float twelve, 
+            float thirteen, float fourteen, float fifteen, float sixteen)
         {
-            m00 = m1; m01 = m5; m02 = m9; m03 = m131;
-            m10 = m2; m11 = m6; m12 = m101; m13 = m14;
-            m20 = m3; m21 = m7; m22 = m111; m23 = m15;
-            m30 = m4; m31 = m8; m32 = m121; m33 = m16;
+            m00 = one; m01 = two; m02 = three; m03 = four;
+            m10 = five; m11 = six; m12 = seven; m13 = eight;
+            m20 = nine; m21 = ten; m22 = eleven; m23 = twelve;
+            m30 = thirteen; m31 = fourteen; m32 = fifteen; m33 = sixteen;
         }
 
         public void WriteMatrix()
@@ -44,19 +47,28 @@ namespace MathClasses
             Console.WriteLine(m30 + " " + m31 + " " + m32 + " " + m33);
         }
 
-        public Matrix4 SetRotateX(float a)
+        public void SetRotateX(float a)
         {
-            return this * new Matrix4(new Vector4(1, 0, 0, 0), new Vector4(0, (float)Math.Cos(a), -(float)Math.Sin(a), 0), new Vector4(0, (float)Math.Sin(a), (float)Math.Cos(a), 0), new Vector4());
+            m00 = 1; m10 = 0; m20 = 0; m30 = 0;
+            m01 = 0; m11 = MathF.Cos(a); m21 = -MathF.Sin(a); m31 = 0;
+            m02 = 0; m12 = MathF.Sin(a); m22 = MathF.Cos(a); m32 = 0;
+            m03 = 0; m13 = 0; m23 = 0; m33 = 1;
         }
 
-        public Matrix4 SetRotateY(float a)
+        public void SetRotateY(float a)
         {
-            return this * new Matrix4(new Vector4((float)Math.Cos(a), 0, (float)Math.Sin(a), 0), new Vector4(0, 1, 0, 0), new Vector4(-(float)Math.Sin(a), 0, (float)Math.Cos(a), 0), new Vector4());
+            m00 = MathF.Cos(a); m10 = 0; m20 = MathF.Sin(a); m30 = 0;
+            m01 = 0; m11 = 1; m21 = 0; m31 = 0;
+            m02 = -MathF.Sin(a); m12 = 0; m22 = MathF.Cos(a); m32 = 0;
+            m03 = 0; m13 = 0; m23 = 0; m33 = 1;
         }
 
-        public Matrix4 SetRotateZ(float a)
+        public void SetRotateZ(float a)
         {
-            return this * new Matrix4(new Vector4((float)Math.Cos(a), -(float)Math.Sin(a), 0, 0), new Vector4((float)Math.Sin(a), (float)Math.Cos(a), 0, 0), new Vector4(0, 0, 1, 0), new Vector4());
+            m00 = MathF.Cos(a); m10 = -MathF.Sin(a); m20 = 0; m30 = 0;
+            m01 = MathF.Sin(a); m11 = MathF.Cos(a); m21 = 0; m31 = 0;
+            m02 = 0; m12 = 0; m22 = 1; m32 = 0;
+            m03 = 0; m13 = 0; m23 = 0; m33 = 1;
         }
 
         public void SetTranslation(float a, float b)
@@ -75,28 +87,20 @@ namespace MathClasses
         // vector transformation
         public static Vector4 operator *(Matrix4 m, Vector4 v)
         {
-            return new Vector4(v.x * m.m00 + v.y * m.m01 + v.z * m.m02 + v.w * m.m03,
-                v.x * m.m10 + v.y * m.m11 + v.z * m.m12 + v.w * m.m13,
-                v.x * m.m20 + v.y * m.m21 + v.z * m.m22 + v.w * m.m23,
-                v.x * m.m30 + v.y * m.m31 + v.z * m.m32 + v.w * m.m33);
+            return new Vector4(v.Dot(new Vector4(m.m00, m.m10, m.m20, m.m30)),
+                v.Dot(new Vector4(m.m01, m.m11, m.m21, m.m31)),
+                v.Dot(new Vector4(m.m02, m.m12, m.m22, m.m32)),
+                v.Dot(new Vector4(m.m03, m.m13, m.m23, m.m33)));
         }
 
         // matrix multiplication
         public static Matrix4 operator *(Matrix4 m1, Matrix4 m2)
         {
-            Vector4 row1 = new Vector4(m1.m00, m1.m01, m1.m02, m1.m03);
-            Vector4 row2 = new Vector4(m1.m10, m1.m11, m1.m12, m1.m13);
-            Vector4 row3 = new Vector4(m1.m20, m1.m21, m1.m22, m1.m23);
-            Vector4 row4 = new Vector4(m1.m30, m1.m31, m1.m32, m1.m33);
-            Vector4 col1 = new Vector4(m2.m00, m2.m10, m2.m20, m2.m30);
-            Vector4 col2 = new Vector4(m2.m01, m2.m11, m2.m21, m2.m31);
-            Vector4 col3 = new Vector4(m2.m02, m2.m12, m2.m22, m2.m32);
-            Vector4 col4 = new Vector4(m2.m03, m2.m13, m2.m23, m2.m33);
-
-            return new Matrix4(new Vector4(row1.Dot(col1), row2.Dot(col1), row3.Dot(col1), row4.Dot(col1)),
-                new Vector4(row1.Dot(col2), row2.Dot(col2), row3.Dot(col2), row4.Dot(col2)),
-                new Vector4(row1.Dot(col3), row2.Dot(col3), row3.Dot(col3), row4.Dot(col3)),
-                new Vector4(row1.Dot(col4), row2.Dot(col4), row3.Dot(col4), row4.Dot(col4)));
+            Vector4 col1 = new Vector4(m2.m00, m2.m01, m2.m02, m2.m03);
+            Vector4 col2 = new Vector4(m2.m10, m2.m11, m2.m12, m2.m13);
+            Vector4 col3 = new Vector4(m2.m20, m2.m21, m2.m22, m2.m23);
+            Vector4 col4 = new Vector4(m2.m30, m2.m31, m2.m32, m2.m33);
+            return new Matrix4(m1 * col1, m1 * col2, m1 * col3, m1 * col4);
         }
     }
 }
